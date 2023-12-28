715560c5ae9a059c35bfac656da8ae4c1ecbcd97
Ryan Wong <ryanjameswong@outlook.com>
2023-12-28 13:32:43 -0500

    Create function to choose winner, then simulate one round against computer

--- app.js
+++ app.js
@@ -4,12 +4,57 @@ function getComputerChoice(){
   return choiceNumber == 0 ? "Rock" : choiceNumber == 1 ? "Paper" : "Scissors"; 
 }
 
-let playerSelection;
-do{
-  playerSelection = prompt("Please enter your choice");
-  playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-}while(typeof(playerSelection) != "string" || (playerSelection != "Rock"
-  && playerSelection != "Paper" && playerSelection != "Scissors"));
 
+function getUserChoice(){
+  let userInput;
+  do {
+    userInput = prompt("Please enter your choice");
+    userInput = userInput.charAt(0).toUpperCase() + userInput.slice(1);
+  } while (typeof(userInput) != "string" || (userInput != "Rock"
+    && userInput != "Paper" && userInput != "Scissors"));
+  
+    return userInput;
+}
 
-console.log("you chose " + playerSelection);
+function chooseWinner(firstSelection, secondSelection){
+  let winningHands = {
+    "Rock" : "Scissors",
+    "Paper" : "Rock",
+    "Scissors" : "Paper"
+  };
+
+  if (winningHands[firstSelection] == secondSelection) {
+    return firstSelection;
+  }
+  else if (winningHands[secondSelection] == firstSelection){
+    return secondSelection;
+  }
+  else return null;
+}
+
+function playRound() {
+  let winner;
+  let playerSelection;
+  let computerSelection;
+  let ties = 0;
+
+  while (!winner){
+    if (ties++ > 0) {
+      console.log("Tie. Choose again");
+    }
+    playerSelection = getUserChoice();
+    computerSelection = getComputerChoice();
+    console.log("you chose " + playerSelection);
+    console.log("computer chose " + computerSelection);
+    winner = chooseWinner(playerSelection, computerSelection);
+  }
+  if (winner == playerSelection) {
+    console.log("You win! " + playerSelection + " beats " + computerSelection);
+  }
+  else {
+    console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+  }
+  return winner;
+}
+
+console.log(playRound());
