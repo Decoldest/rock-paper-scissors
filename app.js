3a01a6210e1640596df81d5b37d8a70ff1ea3706
Ryan Wong <ryanjameswong@outlook.com>
2023-12-28 12:52:39 -0500

    Include prompt for user input choice, continue prompt until valid choice given

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ function getComputerChoice(){
   return choiceNumber == 0 ? "Rock" : choiceNumber == 1 ? "Paper" : "Scissors"; 
 }
 
-let playerSelection = prompt("Please enter your choice");
+let playerSelection;
+do{
+  playerSelection = prompt("Please enter your choice");
+  playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+}while(typeof(playerSelection) != "string" || (playerSelection != "Rock"
+  && playerSelection != "Paper" && playerSelection != "Scissors"));
+
+
 console.log("you chose " + playerSelection);
